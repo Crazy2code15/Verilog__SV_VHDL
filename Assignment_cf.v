8b9d26dd48028e09c1c067c0cccbc313464940b0
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2021-12-09 09:09:00 +0530

    Update and rename 1.v to Assignment_cf.v

--- Assignment_cf.v
+++ Assignment_cf.v
@@ -70,5 +70,37 @@ module cf(
   always@(*) begin
     Y<=y15;
   end
-  
+endmodule
+
+
+//Testbench
+module tb();
+  reg I0, I1, I2, I3, I4, I5, I6, I7, I8;
+  wire Y;
+  cf dut(I0, I1, I2, I3, I4, I5, I6, I7, I8,Y);
+  initial begin
+    $monitor("@time %3d : when input is %b %b %b %b %b %b %b %b %b output is %b",$time,I0,I1,I2,I3,I4,I5,I6,I7,I8,Y);
+    #5 
+    I0=0;
+    I1=1;
+    I2=0;
+    I3=1;
+    I4=0;
+    I5=1;
+    I6=0;
+    I7=1;
+    I8=1;
+    
+    #10
+    I0=1;
+    I1=1;
+    I2=0;
+    I3=1;
+    I4=1;
+    I5=1;
+    I6=0;
+    I7=0;
+    I8=1;
+    $finish;
+  end
 endmodule
