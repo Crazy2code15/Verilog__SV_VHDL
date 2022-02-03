e5f481ccda7b5ede55e3812469bcc85b894e299e
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-02-03 16:38:34 +0530

    Create 2.v

---
+++ 2.v
@@ -0,0 +1,10 @@
+module test;
+  int array_2d[4][3];
+  
+  initial begin array_2d='{'{0,1,2},'{3,4,5}, '{6,7,8}, '{9,10,11}};
+    
+    foreach(array_2d[i,j])
+      $display("array[%0d][%0d]=%d",i,j,array_2d[i][j]);
+    
+  end
+endmodule
