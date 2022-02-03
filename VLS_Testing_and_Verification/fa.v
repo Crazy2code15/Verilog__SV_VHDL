4d5af7f28746c7e4b9da6f05f7f278b7e306f734
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-02-03 15:46:03 +0530

    Update fa.v

--- VLS_Testing_and_Verification/fa.v
+++ VLS_Testing_and_Verification/fa.v
@@ -1,25 +1,37 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    08:31:56 02/03/2022 
-// Design Name: 
-// Module Name:    fa 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+//Design Code
 //////////////////////////////////////////////////////////////////////////////////
 
 module fa(input a,b,c, output s,cy);
 	assign s = a^b^c; //logic for sum
 	assign cy = a&b | b&c | a&c; //logic for carry 
+endmodule
+
+// Testbench Code
+
+module tb;
+
+    // Inputs
+    reg a; b; c; d; s0; s1;
+
+    // Outputs
+    wire y;
+
+    // Instantiate the Unit Under Test (UUT)
+    mux41 uut (.a(a), .b(b), .c(c), .d(d), .s0(s0), .s1(s1), .y(y));
+
+    initial begin
+        // Initialize Inputs
+        a = 0; b = 0; c = 0; d = 1; s0 = 0; s1 = 0;
+        #20;
+        a = 0; b = 0; c = 0; d = 1; s0 = 0; s1 = 1;
+        #20;
+        a = 0; b = 0; c = 0; d = 1; s0 = 1; s1 = 0;
+        #20;
+        a = 0; b = 0; c = 0; d = 1; s0 = 1; s1 = 1;
+        end
+initial begin 
+$monitor ($time , "a=%b, b=%b, c=%b, s0=%b, S1=%b", a,b,c,s0,s1);
+End
 endmodule
