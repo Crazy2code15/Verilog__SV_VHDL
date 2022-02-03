9bd7a6616b8288206f0569d1f0b22305dbeb0f47
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-02-03 15:47:09 +0530

    mux41.v

--- VLS_Testing_and_Verification/mux41.v
+++ VLS_Testing_and_Verification/mux41.v
@@ -1,23 +1,34 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:42:39 01/24/2022 
-// Design Name: 
-// Module Name:    mux41 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Design code 
 //////////////////////////////////////////////////////////////////////////////////
 module mux41(input a, b, c, d, s0, s1, output y);
 assign y=((~(s0))&(~(s1))&a)||((~(s0))&s1&b)||(s0&(~(s1))&c)||(s0&s1&d);
+endmodule
+
+//Testbench Code
+module tb;
+
+    // Inputs
+    reg a; b; c; d; s0; s1;
+
+    // Outputs
+    wire y;
+
+    // Instantiate the Unit Under Test (UUT)
+    mux41 uut (.a(a), .b(b), .c(c), .d(d), .s0(s0), .s1(s1), .y(y));
+
+    initial begin
+        // Initialize Inputs
+        a = 0; b = 0; c = 0; d = 1; s0 = 0; s1 = 0;
+        #20;
+        a = 0; b = 0; c = 0; d = 1; s0 = 0; s1 = 1;
+        #20;
+        a = 0; b = 0; c = 0; d = 1; s0 = 1; s1 = 0;
+        #20;
+        a = 0; b = 0; c = 0; d = 1; s0 = 1; s1 = 1;
+        end
+initial begin 
+$monitor ($time , "a=%b, b=%b, c=%b, s0=%b, S1=%b", a,b,c,s0,s1);
+End
 endmodule
